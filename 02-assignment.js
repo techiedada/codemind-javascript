051bb4581892b6b95d2a403a96a728814579951a
techiedada <mahangaredattatray@gmail.com>
2025-02-27 10:35:08 +0530

    Assignment-02 => Variable declaration, using typeof()  find datatype.
    Assignment-02 => Variable declaration, using typeof()  find datatype.

---
+++ 02-assignment.js
@@ -0,0 +1,20 @@
+var bankName;
+console.log(`Before initialization value of bankName: ${bankName} `);
+console.log(`Before initialization data type of bankName: ${typeof(bankName)} `);
+var bankName = 'SBI Bank';
+console.log(`After initialization value of bankName: ${bankName} `);
+console.log(`After initialization data type of bankName: ${typeof(bankName)} `);
+
+var marks = '90%';
+console.log(`The value of Mark:${marks}, And Data type:${typeof(marks)}`);
+
+var isWorking;
+console.log(`Before initializing value of isWorking:${isWorking}, And Data type:${typeof(isWorking)}`);
+var isWorking = true;
+console.log(`After initializing value of isWorking:${isWorking}, And Data type:${typeof(isWorking)}`);
+
+var totalCount = "Hundred and Seven";
+console.log(`The value of totalCount:${totalCount}, And Data type:${typeof(totalCount)}`);
+var totalCount = 107;
+console.log(`After update value of totalCount:${totalCount}, And Data type:${typeof(totalCount)}`);
+
