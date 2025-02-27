69431dfaf29da1029436904c653ea638e35380e4
techiedada <mahangaredattatray@gmail.com>
2025-02-27 10:27:00 +0530

    Assignment-01 => Variable declaration, initialize and log.
    Assignment-01 => Variable declaration, initialize and log.

---
+++ 01-assignment-variable.js
@@ -0,0 +1,36 @@
+// --- First Program ---
+console.log("--- First Program ---");
+var myFullName;
+console.log("Before initialization:",myFullName);
+var myFullName = "Dattatray Mahangare";
+console.log("After initialization:",myFullName);
+var myFullName = "Dattatray Ashok Mahangare";
+console.log("After Update:",myFullName);
+// --- Second Program ---
+console.log("--- Second Program ---");
+var myLove = "I love only JS";
+console.log("Value of myLove is:",myLove);
+// --- Third Program ---
+console.log("--- Third Program ---");
+var firstName = "Dattatray";
+console.log("firstName:",firstName);
+var lastName = "Mahangare";
+console.log("lastName:",lastName);
+var age = 28;
+console.log("AGE:",age);
+var address = "Narayanpur, Pune.";
+console.log("Address:",address);
+var pinCode = 412301;
+console.log("pinCode:",pinCode);
+var gender = "Male";
+console.log("Gender:",gender);
+var isMarried = "No";
+console.log("Married Status:",isMarried);
+
+
+
+
+
+
+
+
