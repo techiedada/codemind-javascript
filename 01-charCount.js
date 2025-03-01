e01aeec0b9c42d57894b4ee7d334c9b695f4a416
techiedada <mahangaredattatray@gmail.com>
2025-03-01 19:50:20 +0530

    Program: Count total no. of character from string.
    Program: Count total no. of character from string.

---
+++ 01-charCount.js
@@ -0,0 +1,7 @@
+console.log("+++++++++++++++++++++++++++++++++++++++++++");
+
+const sentence = "JavaScript LANGUAGE";
+const sentence1 = sentence.replace(" ","");
+const countChar = sentence1.length;
+console.log("Total No of Character is: "+countChar);
+
