a3d8258b73bff5f64bb1501ed1de1df0002b3039
techiedada <mahangaredattatray@gmail.com>
2025-03-02 16:43:23 +0530

    Program- String revers.
    Program- String revers.

---
+++ 01-string-reverse.js
@@ -0,0 +1,8 @@
+const str = "codemind";
+console.log("Given string is : "+str);
+
+const data = str.split("");
+//console.log(data);
+const ans = data.reverse();
+console.log("Revers of given string is: "+ans.join(""));
+
